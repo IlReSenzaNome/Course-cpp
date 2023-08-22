198929c0eb9cb8116c9a7775664db809a4b2b8a4
IlReSenzaNome <alexisvivas3@gmail.com>
2023-08-22 11:44:35 -0500

    renamed file tipe and configuration .gitignore

--- Lists/Insertar-elementos-en-una-lista-enlazada.cpp
+++ Lists/Insertar-elementos-en-una-lista-enlazada.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-struct Nodo {
+struct Nodo
+{
     int dato;
     Nodo *siguiente;
 };
@@ -17,31 +18,38 @@ int main()
     int dato;
     do
     {
-        cout<<"Ingrese un dato: ";
-        cin>>dato;
+        cout << "Ingrese un dato: ";
+        cin >> dato;
         insertarlista(lista, dato);
     } while (dato != 0);
-    
+
     getch();
     return 0;
 }
 
-void insertarlista(Nodo *&lista,int n) {
+void insertarlista(Nodo *&lista, int n)
+{
     Nodo *nuevo_nodo = new Nodo();
     nuevo_nodo->dato = n;
+
     Nodo *aux1 = lista;
     Nodo *aux2;
 
-    while ((aux1!= NULL) && (aux2->dato < n)) {
+    while ((aux1 != NULL) && (aux1->dato < n))
+    {
         aux2 = aux1;
         aux1 = aux1->siguiente;
     }
 
-    if (lista == aux1) {
+    if (lista == aux1)
+    {
         lista = nuevo_nodo;
-    } else {
+    }
+    else
+    {
         aux2->siguiente = nuevo_nodo;
     }
+
     nuevo_nodo->siguiente = aux1;
-    cout<<endl<<"El elemento "<<n<<" fue ingresado corectamente "<<endl<<endl;
+    cout << "El elemento " << n << " fue ingresado corectamente " << endl;
 }
---
+++ Lists/Mostrar-elementos-en-una-lista-enlazada.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+struct Nodo
+{
+    int dato;
+    Nodo *siguiente;
+};
+
+void menu();
+void insertarlista(Nodo *&, int);
+void mostrarlista(Nodo *);
+
+int main()
+{
+
+    menu();
+
+    getch();
+    return 0;
+}
+
+void menu()
+{
+    Nodo *lista = NULL;
+    int opcion, dato;
+
+    do
+    {
+        cout << "\t..MENU..\t" << endl;
+        cout << "\t..1. Insertar elementos a la lista: " << endl;
+        cout << "\t..2. Mostrar los elementos de la lista: " << endl;
+        cout << "\t..3. Salir: " << endl;
+        cout << "Opcion: ";
+        cin >> opcion;
+        switch (opcion)
+        {
+        case 1:
+            do
+            {
+                cout << "Ingrese un dato: ";
+                cin >> dato;
+                insertarlista(lista, dato);
+            } while (dato != 0);
+            system("pause");
+            break;
+        case 2:
+            mostrarlista(lista);
+            system("pause");
+            break;
+        case 3:
+            cout << "Adios";
+            break;
+        default:
+            cout << "Opcion no existente intentalo denuevo";
+            break;
+        }
+        system("cls");
+    } while (opcion != 3);
+}
+
+void insertarlista(Nodo *&lista, int n)
+{
+    Nodo *nuevo_nodo = new Nodo();
+    nuevo_nodo->dato = n;
+
+    Nodo *aux1 = lista;
+    Nodo *aux2;
+
+    while ((aux1 != NULL) && (aux1->dato < n))
+    {
+        aux2 = aux1;
+        aux1 = aux1->siguiente;
+    }
+
+    if (lista == aux1)
+    {
+        lista = nuevo_nodo;
+    }
+    else
+    {
+        aux2->siguiente = nuevo_nodo;
+    }
+
+    nuevo_nodo->siguiente = aux1;
+    cout << "El elemento " << n << " fue ingresado corectamente " << endl;
+}
+
+void mostrarlista(Nodo *lista)
+{
+    Nodo *actual = new Nodo();
+
+    actual = lista;
+    while (actual != NULL)
+    {
+        cout << actual->dato << " -> ";
+        actual = actual->siguiente;
+    }
+}
